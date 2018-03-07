0f2c2451322b7aead3e2e666706500e02823a14b
Karina Ionkina <kionkina@stuy.edu>
2018-03-06 22:00:22 -0500

    to do: animation stuff

--- svg.js
+++ svg.js
@@ -1,6 +1,23 @@
+
 var pic = document.getElementById("vimage");
 
 var beep = document.getElementById("beep");
+
+
+var create_dot_random = function(){
+    var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c1.setAttribute("fill", "red");
+    var x = Math.floor(Math.random() * Math.floor(600));
+    var y = Math.floor(Math.random() * Math.floor(600));
+    c1.setAttribute("cx", x);
+    c1.setAttribute("cy", y);
+    c1.setAttribute("r", 10)
+	pic.appendChild(c1);
+
+    c1.addEventListener("click", change_color, true);
+}
+
+
 
 
 var create_dot = function(e){
@@ -14,7 +31,7 @@ var create_dot = function(e){
     c1.setAttribute("cy", e.offsetY);
     c1.setAttribute("r", 10)
     pic.appendChild(c1);
-    c1.removeEventListener("click", create_dot, true);
+
     c1.addEventListener("click", change_color, true); 
     }
     else {
@@ -30,6 +47,8 @@ var create_dot = function(e){
 	console.log("LOGGING THIS...");
 	console.log(this);
 	this.setAttribute("fill","blue");
+	this.addEventListener("click", function(){pic.removeChild(this); create_dot_random();});
+	
 
     }
 
